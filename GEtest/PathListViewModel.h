030a6afb8d536a17dcb02ab2c6ea3b4ad060d3e1
Mohamed Alsadek <mohamedalsadek@VISs-MacBook-Pro.local>
2016-06-18 15:47:50 +0200

    - adding pathlist viewModel

---
+++ GEtest/PathListViewModel.h
@@ -0,0 +1,13 @@
+//
+//  PathListViewModel.h
+//  GEtest
+//
+//  Created by Mohamed Alsadek on 6/18/16.
+//  Copyright © 2016 Mohamed Alsadek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PathListViewModel : NSObject
+
+@end
---
+++ GEtest/PathListViewModel.m
@@ -0,0 +1,13 @@
+//
+//  PathListViewModel.m
+//  GEtest
+//
+//  Created by Mohamed Alsadek on 6/18/16.
+//  Copyright © 2016 Mohamed Alsadek. All rights reserved.
+//
+
+#import "PathListViewModel.h"
+
+@implementation PathListViewModel
+
+@end
