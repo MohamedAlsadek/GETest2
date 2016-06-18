bc9d1251c9aa21f91192409fb8f33046f0dad9a7
Mohamed Alsadek <mohamedalsadek@VISs-MacBook-Pro.local>
2016-06-18 21:51:32 +0200

    hadnling (+1) on arrival and departure time

--- GEtest/PathListTableViewCell.m
+++ GEtest/PathListTableViewCell.m
@@ -24,10 +24,32 @@
 
 
 - (void)initCellWithPath:(Path *)path {
-    self.labelStartEndTime.text = [NSString stringWithFormat:@"%@ - %@", path.departureTime, path.arrivalTime];
+    [self setLabelStartEndTimeWithObject:path];
     [self setDurationLabelWithObject:path];
     [self setPriceLabelWithObject:path];
     [self setLogoWithObject:path];
+}
+
+- (void)setLabelStartEndTimeWithObject:(Path *)path {
+    
+    int departure = [self getHourValueFromStringFormat:path.departureTime];
+    int arrival = [self getHourValueFromStringFormat:path.arrivalTime];
+    
+    if (arrival < departure) {
+        self.labelStartEndTime.text = [NSString stringWithFormat:@"%@ - %@ (+1)", path.departureTime, path.arrivalTime];
+    }else {
+        self.labelStartEndTime.text = [NSString stringWithFormat:@"%@ - %@", path.departureTime, path.arrivalTime];
+    }
+}
+
+// hh:mm
+- (int)getHourValueFromStringFormat:(NSString *)format {
+    NSArray *components = [format componentsSeparatedByString:@":"];
+    if (components.count == 2) { // it means the string is valid
+        return [components.firstObject intValue];
+    }
+    
+    return -1;
 }
 
 - (void)setDurationLabelWithObject:(Path *)path {
